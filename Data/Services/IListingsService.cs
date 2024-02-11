013965cc609c324c97f49810ce328473b49bb441
Lark3s <lu.spasojevic@gmail.com>
2024-02-11 07:27:39 +0100

    Add project files.

---
+++ Data/Services/IListingsService.cs
@@ -0,0 +1,12 @@
+﻿using AuctionsApp.Models;
+
+namespace AuctionsApp.Data.Services
+{
+    public interface IListingsService
+    {
+        IQueryable<Listing> GetAll();
+        Task Add(Listing listing);
+        Task<Listing> GetById(int? id);
+        Task SaveChanges();
+    }
+}
